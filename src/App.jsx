48f62211cccf85e6f5ad807c19d821f81d07367a
Rayala07 <rayala07viswanath@Gmail.com>
2026-01-26 11:31:27 +0530

    Implemented Icons Dock

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Dock  from "./components/Dock.jsx"
 
 const App = () => {
   return (
-    <main className='bg-[url(D:\mac-os\public\mac-wallpaper.jpg)] bg-cover bg-center'>
+    <main className='bg-[url(/mac-wallpaper.jpg)] bg-cover bg-center h-screen '>
       <Dock />
     </main>
   )
--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,22 +1,26 @@
-import { RiCalendar2Fill, RiFilePdf2Line, RiGithubFill, RiLinksFill, RiMailFill, RiSpotifyFill, RiStickyNoteFill, RiTerminalBoxLine } from "@remixicon/react"
+import { RiCalendar2Fill, RiFilePdf2Line, RiGithubFill, RiLinksFill, RiMailFill, RiSpotifyFill, RiStickyNoteFill, RiTerminalLine } from "@remixicon/react"
 
 const Dock = () => {
+
+  const iconHover = "hover:scale-[1.2] hover:-translate-y-2 transition-all ease-out duration-300";
+
   return (
-    <footer className="flex gap-2.5
-      absolute bottom-4 left-1/2 -translate-x-1/2
-      px-6 py-4
-      bg-white/10
-      backdrop-blur-md
-      border border-white/30
-      rounded-2xl">
-      <div><RiGithubFill /></div>
-      <div><RiCalendar2Fill /></div>
-      <div><RiTerminalBoxLine /></div>
-      <div><RiStickyNoteFill /></div>
-      <div><RiFilePdf2Line /></div>
-      <div><RiSpotifyFill /></div>
-      <div><RiMailFill /></div>
-      <div><RiLinksFill /></div>
+    <footer className="flex gap-4 items-center
+            absolute bottom-4 left-1/2 -translate-x-1/2
+            px-6 py-3
+            bg-[#47474756]
+            backdrop-blur-xl
+            rounded-xl
+            "
+    >
+        <RiGithubFill className={`w-9.5 h-9.5 text-white bg-black p-1 rounded-md ${iconHover}`} />
+        <RiCalendar2Fill className={`w-9 h-9 text-white bg-linear-to-br from-blue-600 to-blue-400 p-1 rounded-md ${iconHover}`}/>
+        <RiTerminalLine className={`w-10 h-10 text-white bg-black p-1 rounded-md ${iconHover}`} />
+        <RiStickyNoteFill className={`w-9 h-9 text-white bg-linear-to-br from-orange-400 to-orange-300 p-1 rounded-md ${iconHover}`} />
+        <RiFilePdf2Line className={`w-9 h-9 text-white bg-red-500 p-1 rounded-md ${iconHover}`} />
+        <RiSpotifyFill className={`w-9 h-9 text-white bg-green-600 p-1 rounded-md ${iconHover}`} />
+        <RiMailFill className={`w-9.5 h-9 text-white bg-linear-to-br from-blue-600 to-blue-400 p-1 rounded-md ${iconHover}`} />
+        <RiLinksFill className={`w-9 h-9 text-white bg-linear-to-br from-purple-700 to-purple-500 p-1 rounded-md ${iconHover}`} />
     </footer>
   )
 }
